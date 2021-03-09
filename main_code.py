ea214f4acb571f386748f2b80c6444f85745a510
JS <jsedrak@student.ie.edu>
2021-03-09 09:32:03 -0500

    Class 7 on Dictionary and Modules

--- main_code.py
+++ main_code.py
@@ -1,4 +1,11 @@
 # we want to use the module
-import module
+#import module
+import module as external_module
+import module2
+#print(module.b)
+print(f"b = {external_module.b}")
+print(f"b - {module2.b}")
 
-print(module.b)
+# I want to call the function in the module:
+#module.function_in_module()
+external_module.function_in_module()
